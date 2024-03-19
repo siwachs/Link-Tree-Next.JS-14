61d2f94b7edac0758077eb138203b9a1a92af266
siwachs <ssiwach.8888@gmail.com>
2024-03-19 11:26:41 +0530

    Header and Hero Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,34 @@
 import Header from "@/components/Header";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
       <Header />
-      <section></section>
+
+      <section className="p-6 pt-32 max-w-7xl mx-auto">
+        <div className="max-w-md mb-6">
+          <h1 className="text-6xl font-bold">
+            Your one link <br /> for everything
+          </h1>
+          <h2 className="text-gray-500 text-xl mt-6">
+            Share your links, music, social profiles, contact information and
+            more on one page
+          </h2>
+        </div>
+
+        <form className="inline-flex items-center shadow-lg">
+          <span className="bg-white py-4 pl-4">linktree.to/</span>
+          <input
+            placeholder="username"
+            className="py-4 px-2 outline-none"
+            type="text"
+          />
+          <button className="bg-blue-500 text-white py-4 px-6" type="submit">
+            Join for Free
+          </button>
+        </form>
+      </section>
     </main>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,21 +3,20 @@ import React from "react";
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-white border-b flex justify-between p-4">
-      <div className="flex gap-6">
-        <Link href="/">LinkTree</Link>
+    <header className="bg-white border-b py-4">
+      <div className="max-w-7xl mx-auto flex justify-between px-6">
+        <Link href="/" className="text-blue-500 font-bold text-xl">
+          LinkTree
+        </Link>
 
-        <nav className="flex items-center gap-4 text-slate-500 text-sm">
+        <nav className="flex gap-4 text-sm text-slate-500">
           <Link href="/about">About</Link>
           <Link href="/pricing">Pricing</Link>
           <Link href="/contact">Contact</Link>
+          <Link href="/signin">Sign in</Link>
+          <Link href="/register">Create Account</Link>
         </nav>
       </div>
-
-      <nav className="flex gap-4 text-sm text-slate-500">
-        <Link href="/signin">Sign in</Link>
-        <Link href="/register">Register</Link>
-      </nav>
     </header>
   );
 };
