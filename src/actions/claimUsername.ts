b1907e21112fa0f4760f18927192a88cde49b60a
siwachs <ssiwach.8888@gmail.com>
2024-03-23 12:20:33 +0530

    ER Diagram and Use Server Side Form Handling

---
+++ src/actions/claimUsername.ts
@@ -0,0 +1,5 @@
+"use server";
+
+export default async function claimUsername(formData: any) {
+  const username = formData.get("username");
+}
--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,6 +1,8 @@
 import { redirect } from "next/navigation";
 import { authOption } from "@/app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
+import RightIcons from "@/components/icons/RightIcons";
+import claimUsername from "@/actions/claimUsername";
 
 export default async function AccountPage(req: any) {
   // @ts-ignore
@@ -10,23 +12,25 @@ export default async function AccountPage(req: any) {
 
   return (
     <div>
-      <form>
+      <form action={claimUsername}>
         <h1 className="text-4xl font-bold text-center select-none mb-6">
           Grab your username
         </h1>
 
         <div className="max-w-sm mx-auto">
           <input
+            name="username"
             defaultValue={req.searchParams?.desiredUsername}
             className="block p-2 outline-none mx-auto border w-full mb-2 text-center"
             type="text"
             placeholder="Username"
           />
           <button
-            className="bg-blue-500 text-white py-2 px-4 block mx-auto w-full"
+            className="bg-blue-500 text-white py-2 px-4 mx-auto w-full flex gap-2 items-center justify-center"
             type="submit"
           >
-            Claim this username
+            <span>Claim this username</span>
+            <RightIcons />
           </button>
         </div>
       </form>
