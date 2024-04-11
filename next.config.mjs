b8c1ff70d121ce4604320ce93fe1ca6b1204a2df
siwachs <ssiwach.8888@gmail.com>
2024-04-11 21:30:10 +0530

    Fix CORS

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,15 @@
+const headers = [
+  "Accept",
+  "Accept-Version",
+  "Content-Length",
+  "Content-MD5",
+  "Content-Type",
+  "Date",
+  "X-Api-Version",
+  "X-CSRF-Token",
+  "X-Requested-With",
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -8,6 +20,22 @@ const nextConfig = {
         hostname: "linktree-clone-next14.s3.amazonaws.com",
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/(.*)",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}`,
+          },
+          { key: "Access-Control-Allow-Methods", value: "GET,POST" },
+          { key: "Access-Control-Allow-Headers", value: headers.join(", ") },
+        ],
+      },
+    ];
   },
 };
 
